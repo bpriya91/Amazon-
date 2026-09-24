661e4b17a62592dda42344c25ef7db7cce686047
bpriya91 <bpriyadharsini91@gmail.com>
2026-09-24 15:48:30 +0530

    changes done

--- tests/Frameshandling.spec.js
+++ tests/Frameshandling.spec.js
@@ -1,3 +1,15 @@
+// import test from'@playwright/test';
+
+// test('Frames handling', async ({ page }) => {
+//     await page.goto('https://www.globalsqa.com/demo-site/frames-and-windows/#iFrame');
+//  const frame =  page.frameLocator('[name="globalSqa]');
+//  await frame.getByText("//h3[text()='JMeter Training']").click
+// await page.waitForTimeout(3000);
+
+// })
+
+
+
 import test from'@playwright/test';
 
 test('Frames handling', async ({ page }) => {
